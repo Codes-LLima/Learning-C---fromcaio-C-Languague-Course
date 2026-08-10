5fc7f689f11305fa42dda2eb75b7f15fc7360124
Lucas Lima <zsoakinho@gmail.com>
2026-08-09 23:22:47 -0300

    Variables Types

---
+++ lg C 03 - tiposDeVariaveis/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void main(){
+    //Caracteres
+    char nome_da_variavel; 
+    nome_da_variavel = 'a'; // -128 até 127
+    unsigned char variavel; // 0 até 255
+    
+    //Números inteiros (Conjunto dos inteiros)
+    short int nome_da_variavel2;
+    int nome_da_variavel3;
+    long nome_da_variavel4;
+
+    //Números inteiros apenas positivos (conjunto dos números naturais)
+    unsigned short int;
+    unsigned int;
+    unsigned long int;
+
+    //Números reais
+    float nome_da_variavel5;
+    double nome_da_variavel6;
+    long double nome_da_variavel7;
+
+    //Constante nomeada
+    const int max = 100;
+    printf()
+}
